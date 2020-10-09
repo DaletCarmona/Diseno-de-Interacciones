efffc3e9064442bd18718467f148c73413ad2854
Dalet <dalet.carmona@uabc.edu.mx>
2020-10-09 16:56:24 -0700

    Create Program.cs

---
+++ Práctica 2/Problema/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problema_Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var;
+            string[] lista = {"MUFASA", "SCAR", "SIMBA", "NALA", "KIARA", "SARABI", "PUMBAA", "TIMON", "ZAZU", "KOBU"};
+            string nombre;
+    
+            nombre=Console.ReadLine();
+            var=Compare(lista, nombre);
+
+            if (var == 1)
+                Console.WriteLine("Nombre en Lista");
+            else
+                Console.WriteLine("Nombre no en Lista");
+        }
+
+        static int Compare(string[] lista, string nombre) 
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+
+                if (String.Equals(lista[i], nombre.ToUpper()))
+                    return 1;
+            }
+
+            return 0;
+        }
+    }
+}
