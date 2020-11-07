724c9bbd3d2a08bc1e64e78bdfa7631c60918a9f
Dalet <dalet.carmona@uabc.edu.mx>
2020-11-06 22:49:01 -0800

    Protected Instance
    Se añadió un Nuevo instance para evitar usar el Find.

---
+++ Práctica 2/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PocionHP.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Nueva Pocion", menuName = "Inventario/Pocion/PocionHP")]
+
+public class PocionHP : Item
+{
+    // Start is called before the first frame update
+    public override void Use()
+    {
+        Debug.Log("Curando 50 HP.");
+    }
+}
