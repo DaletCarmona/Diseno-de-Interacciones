d097422d9dd52865dd88250206cdab09a88a9aaa
Dalet <dalet.carmona@uabc.edu.mx>
2020-11-13 20:24:41 -0800

    Problema de Programación
    Se añadió el problema al repositorio.

---
+++ Práctica 2/Problema/Program5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Problema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var;
+            int[] arreglo = { 10, 153, 1548, 95366, 154256 };
+            var=Compare(arreglo);
+
+            Console.Out.WriteLine(var);
+
+        }
+
+        static int Compare(int[] arreglo)
+        {
+            int x = 0;
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if( (arreglo[i].ToString()).Length%2==0 )
+                {
+                    x++;
+                }
+
+            }
+
+            return x;
+        }
+    }
+}
+
